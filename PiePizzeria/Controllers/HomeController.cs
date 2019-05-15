a8f38e391f539a5f008786243d2e104364262d04
travisredd <travisredd@mail.weber.edu>
2019-05-14 21:36:14 -0600

    ...

---
+++ PiePizzeria/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PiePizzeria.Models;
+using PiePizzeria.ViewModels;
+
+namespace PiePizzeria.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IPieRepository _pieRepository;
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pies = _pieRepository.GetAllPies().OrderBy(p => p.Name);
+
+            var homeViewModel = new HomeViewModel()
+            {
+                Pies = pies.ToList(),
+            Title = "Welcome to the Pie Pizzeria!"
+        };
+            return View(homeViewModel);
+        }
+    }
+}
